8e8d73f5eb8b4b3bbbb544f737c3ba10a57fcb67
haydenrh77 <hayden.hackler@gmail.com>
2024-01-04 03:43:25 -0700

    Fixed update checking

--- StardewModdingAPI.bat
+++ StardewModdingAPI.bat
@@ -1,2 +1,11 @@
-start cmd /k Call ".\update.bat"
+powershell.exe -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/haydenrh77/StardewMods/main/version -OutFile version"
+set /p curr=< currentVersion 
+set /p new=< version
+
+if %new% gtr %curr% (
+	echo update available
+	start cmd /k Call ".\update.bat"
+)
+
+
 start cmd /k Call ".\StardewModdingAPI.exe"
