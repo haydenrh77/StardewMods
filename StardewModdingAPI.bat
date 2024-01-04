978bbe25009c082979ecf55d8bf35b0d2ba92c26
haydenrh77 <hayden.hackler@gmail.com>
2024-01-04 02:52:21 -0700

    update.bat and StardewModdingAPI.bat

---
+++ StardewModdingAPI.bat
@@ -0,0 +1,2 @@
+start cmd /k Call ".\update.bat"
+start cmd /k Call ".\StardewModdingAPI.exe"
---
+++ update.bat
@@ -0,0 +1,17 @@
+@echo off
+
+if not exist "PortableGit\" (
+    powershell -Command "Invoke-WebRequest https://github.com/git-for-windows/git/releases/download/v2.43.0.windows.1/PortableGit-2.43.0-64-bit.7z.exe -OutFile PortableGit-2.39.2-64-bit.7z.exe"
+	.\PortableGit-2.39.2-64-bit.7z.exe -o "PortableGit" -y
+    del PortableGit-2.39.2-64-bit.7z.exe
+	.\PortableGit\bin\git.exe init .
+	.\PortableGit\bin\git.exe remote add origin https://github.com/haydenrh77/StardewMods.git
+	.\PortableGit\bin\git.exe fetch --all
+	.\PortableGit\bin\git.exe reset --hard origin/main
+	
+	.\PortableGit\bin\git.exe pull origin main
+)
+
+.\PortableGit\bin\git.exe pull origin main
+
+exit
